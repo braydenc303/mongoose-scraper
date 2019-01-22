da97394bcbc45f90d56b67abff7c40d7b5160b4a
Brayden Copley <braydenc303@gmail.com>
2019-01-21 17:15:01 -0700

    Scrape is working, and data headlines are being stored to the database. Able to res.json data to the front. Not yet able to display in handlebars.

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 //Exporting an object containing all of the models
 
 module.exports = {
-    headline: require("./headline"),
-    note: require("./note")
+    Headline: require("./headline"),
+    Note: require("./note")
 }
